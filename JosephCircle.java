c5e4d5c26eb674e6a044067c12b02c825482c031
jnissenbaum01 <jnissenbaum@brandeis.edu>
2018-02-05 09:37:25 -0500

    class

---
+++ JosephCircle.java
@@ -0,0 +1,15 @@
+package pa03;
+
+public class JosephCircle extends CircleShape{
+
+  public static void main (String[] args){
+    JosephCircle x = new JosephCircle();
+    System.out.println(x);
+  }
+
+  public String toString(){
+    return "JosephCircle"+super.toString();
+  }
+
+
+}
