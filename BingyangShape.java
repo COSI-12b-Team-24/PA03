e2b80c9d4870f859a913b2f1d0646e225a3d5938
byye@brandeis.edu <32751276+bingyanggg@users.noreply.github.com>
2018-02-12 20:36:57 -0500

    update

--- BingyangShape.java
+++ BingyangShape.java
@@ -1,37 +1,12 @@
-<<<<<<< HEAD
 package pa03;
 
-||||||| merged common ancestors
-=======
-package pa03;
-import java.awt.Color;
-
->>>>>>> 9ed0c852389ccee75e3e891816522b398c366276
 public class BingyangShape extends CircleShape {
-<<<<<<< HEAD
   public java.awt.Color borderColor;
-||||||| merged common ancestors
-  public double borderColor;
-=======
-  public Color borderColor;
->>>>>>> 9ed0c852389ccee75e3e891816522b398c366276
 
-<<<<<<< HEAD
   public BingyangShape(){
     super();
     this.color = new java.awt.Color(100,100,100,100);
     this.borderColor = new java.awt.Color(100*(int)Math.random() + 50*(int)Math.random(), 100*(int)Math.random() + 50*(int)Math.random(), 100*(int)Math.random() + 50*(int)Math.random(), 100*(int)Math.random() + 50*(int)Math.random());
-||||||| merged common ancestors
-  public BingyangShape( double x, double y, int radius){
-    super(x, y, radius);
-    this.color = new Color(100,100,100,100);
-    this.borderColor = new Color(100*Math.random() + 50*Math.random, 100*Math.random() + 50*Math.random, 100*Math.random() + 50*Math.random, 100*Math.random() + 50*Math.random);//generate random color for the border
-=======
-  public BingyangShape( double x, double y, int radius){
-    super(x, y, radius);
-    this.color = new Color(100,100,100,100);
-    this.borderColor = new Color((int)(100*Math.random() + 50*Math.random()), (int)(100*Math.random() + 50*Math.random()), (int)(100*Math.random() + 50*Math.random()), (int)(100*Math.random() + 50*Math.random()));//generate random color for the border
->>>>>>> 9ed0c852389ccee75e3e891816522b398c366276
   }
 
   public void keepOnBoard(){
