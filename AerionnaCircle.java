932a08418468fb272e79e4e365fade3e367fd725
Aerionnas <34111182+Aerionnas@users.noreply.github.com>
2018-02-12 23:18:26 -0500

    Add files via upload

---
+++ AerionnaCircle.java
@@ -0,0 +1,13 @@
+package pa03;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class AerionnaCircle extends CircleShape{
+private Color boarderColor;
+public AerionnaCircle(){
+super();
+// this.boarderColor=bC;
+this.color= new java.awt.Color(196,31,30);
+
+}
+}
